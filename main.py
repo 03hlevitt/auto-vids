7e95946347ad2464f27c1eca05612d50ad810b4d
03hlevitt <03hlevitt@gmail.com>
2022-12-28 21:44:47 +0000

    speed and file size

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import glob
 from moviepy.editor import *
 from upload import upload
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Animals= "Alligator,Anteater,Ape,Armadillo,Baboon,Bat,Bear,Beetle,Bongo,Camel,Centipede,Chameleon,Cheetah,Clownfish,Coati,Cockatoo,Crane,Crocodile,Deer,Drill,Duck,Eagle,Echidna,Elephant,Elk,Flamingo,Fox,Frigatebird,Gila monster,Giraffe,Gorilla,Guanaco,Hamster,Hawk,Hedgehog,Hermit crab,Hippo,Hippopotamus,Horse,Hummingbird,Hyena,Iguana,Impala,Jaguar,Kangaroo,Kingfisher,Kite,Kiwi,Koala,Komodo dragon,Kudu,Lemur,Leopard,Lion,Lionfish,Lizard,Lynx,Mole,Monkey,Newt,Nilgai,Numbat,Okapi,Opossum,Orangutan,Ostrich,Owl,Panda,Panther,Parrot,Peacock,Pelican,Penguin,Pigeon,Platypus,Puffin,Quail,Rabbit,Rattlesnake,Red panda,Reindeer,Rhinoceros,Rooster,Scorpion,Seal,Skunk,Snake,Sparrow,Squirrel,Swan,Toucan,Tiger,Turkey,Turtle,Vulture,Walrus,Wolf,Woodpecker,Yak,Zebra"
 
@@ -25,7 +25,7 @@ def create_script(subject):
 
         model='text-davinci-003',
 
-        prompt='Write a 100 word Youtube video script about a ' + subject + ' with voiceovers and 5 short scene descriptions',
+        prompt='Write a 100 word Youtube video script about a ' + subject + ' with voiceovers and 3 short scene descriptions',
 
         temperature=0.7,
 
